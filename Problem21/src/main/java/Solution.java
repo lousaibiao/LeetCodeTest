7ab588723cb09b6628fdaae067b34ac5f7b71dc4
lousaibiao@live.cn <lousaibiao@live.cn>
2019-03-25 23:46:32 +0800

    Problem 21 再理一下思路

---
+++ Problem21/src/main/java/Solution.java
@@ -0,0 +1,12 @@
+class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null && l2 != null) return l2;
+        if (l1 != null && l2 == null) return l1;
+        if (l1 == null && l2 == null) return null;
+
+        ListNode p=l1,q = l2;
+        
+
+        return null;
+    }
+}
---
+++ Problem21/src/test/java/SolutionTest.java
@@ -0,0 +1,5 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+public class SolutionTest {
+}
