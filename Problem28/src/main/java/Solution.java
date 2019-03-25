18e502b5fadd55c28a690869d4da8fd3fa55c1a2
lousaibiao <lousaibiao@live.cn>
2019-03-25 13:32:02 +0800

    Problem28 用了Arrays.copyOfRange，快不少

--- Problem28/src/main/java/Solution.java
+++ Problem28/src/main/java/Solution.java
@@ -17,4 +17,19 @@ public class Solution {
         }
         return -1;
     }
+
+    public int strStrSubString(String haystack, String needle) {
+        if (needle == null || needle.equals("")) return 0;
+        if (needle.length() > haystack.length()) return -1;
+
+        for (int i = 0; i < haystack.length() && i + needle.length() <= haystack.length(); i++) {
+            if (haystack.substring(i, i + needle.length()).equals(needle)) return i;
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        String s = "aa";
+        s.substring(0, 1);
+    }
 }
--- Problem28/src/test/java/SolutionTest.java
+++ Problem28/src/test/java/SolutionTest.java
@@ -20,4 +20,29 @@ class SolutionTest {
         Assertions.assertEquals(0, solution.strStr("hello", "hello"));
     }
 
+    @Test
+    void strStrSubString2() {
+        Assertions.assertEquals(2, solution.strStrSubString("hello", "ll"));
+    }
+
+    @Test
+    void strStrSubStringMinus1() {
+        Assertions.assertEquals(-1, solution.strStrSubString("aaaaa", "bba"));
+    }
+
+    @Test
+    void strStrSubString0() {
+        Assertions.assertEquals(0, solution.strStrSubString("hello", "hello"));
+    }
+
+    @Test
+    void strStrSubString2Too(){
+        Assertions.assertEquals(2,solution.strStrSubString("aabbc","bbc"));
+    }
+
+    @Test
+    void strStrSubStringMinus1Too(){
+        Assertions.assertEquals(-1,solution.strStrSubString("aabbc","bbcc"));
+    }
+
 }
