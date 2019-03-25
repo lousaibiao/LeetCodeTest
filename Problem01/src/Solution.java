9cab72db3b3b69d5aba0f797a48e77353ff9aac7
lousaibiao <lousaibiao@live.cn>
2019-03-25 10:51:35 +0800

    提交第一题

---
+++ Problem01/src/Solution.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+class Solution {
+    public static int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+
+        return new int[]{0, 0};
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{2, 7, 11, 15};
+        int target = 9;
+        System.out.println(Arrays.toString(twoSum(nums, 9)));
+
+    }
+}
+
