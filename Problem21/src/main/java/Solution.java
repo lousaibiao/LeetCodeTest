91b49c459faab66dc2376e5e4420584811a366ab
lousaibiao <lousaibiao@live.cn>
2019-03-26 10:30:49 +0800

    Problem21

--- Problem21/src/main/java/Solution.java
+++ Problem21/src/main/java/Solution.java
@@ -3,10 +3,37 @@ class Solution {
         if (l1 == null && l2 != null) return l2;
         if (l1 != null && l2 == null) return l1;
         if (l1 == null && l2 == null) return null;
+        ListNode firstParent = null, curParent = null;
+        if (l1.val <= l2.val) {
+            firstParent = l1;
+            l1 = l1.next;
+        } else {
+            firstParent = l2;
+            l2 = l2.next;
+        }
+        curParent = firstParent;
+        while (l1 != null && l2 != null) {
+            if (l1.val <= l2.val) {
+                curParent.next = l1;
+                curParent = curParent.next;
+                l1 = l1.next;
+            } else {
+                curParent.next = l2;
+                curParent = curParent.next;
+                l2 = l2.next;
+            }
+        }
+        if (l1 != null) curParent.next = l1;
+        if (l2 != null) curParent.next = l2;
 
-        ListNode p=l1,q = l2;
-        
+        return firstParent;
+    }
 
-        return null;
+    public void showNodes(ListNode node) {
+        while (node != null) {
+            System.out.print(node.val + ",");
+            node = node.next;
+        }
+        System.out.println();
     }
 }
--- Problem21/src/test/java/SolutionTest.java
+++ Problem21/src/test/java/SolutionTest.java
@@ -2,4 +2,57 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 public class SolutionTest {
+    private final Solution solution = new Solution();
+    @Test
+    void mergeTwoListsEqual(){
+        ListNode node1 = new ListNode(1);
+        ListNode node2 = new ListNode(2);
+        ListNode node3 = new ListNode(4);
+        node1.next = node2;
+        node2.next = node3;
+        ListNode node4 = new ListNode(1);
+        ListNode node5 = new ListNode(2);
+        ListNode node6 = new ListNode(4);
+        node4.next = node5;
+        node5.next = node6;
+
+        ListNode mergedNode = solution.mergeTwoLists(node1,node4);
+        solution.showNodes(mergedNode);
+    }
+
+    @Test
+    void mergeTwoListsFirstLong(){
+        ListNode node1 = new ListNode(1);
+        ListNode node2 = new ListNode(2);
+        ListNode node3 = new ListNode(4);
+        node1.next = node2;
+        node2.next = node3;
+        ListNode node4 = new ListNode(9);
+//        ListNode node5 = new ListNode(2);
+//        ListNode node6 = new ListNode(4);
+//        node4.next = node5;
+//        node5.next = node6;
+
+        ListNode mergedNode = solution.mergeTwoLists(node1,node4);
+        solution.showNodes(mergedNode);
+    }
+    @Test
+    void mergeTwoListsSecondLong(){
+        ListNode node1 = new ListNode(1);
+//        ListNode node2 = new ListNode(2);
+//        ListNode node3 = new ListNode(4);
+//        node1.next = node2;
+//        node2.next = node3;
+        ListNode node4 = new ListNode(1);
+        ListNode node5 = new ListNode(2);
+        ListNode node6 = new ListNode(4);
+        node4.next = node5;
+        node5.next = node6;
+
+        ListNode mergedNode = solution.mergeTwoLists(node1,node4);
+        solution.showNodes(mergedNode);
+    }
+
+
+
 }
