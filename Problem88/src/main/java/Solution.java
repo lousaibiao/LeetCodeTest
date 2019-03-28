1c3b2e0f0610440e457536f3fc94e029fc0c845a
lousaibiao <lousaibiao@live.cn>
2019-03-28 09:54:29 +0800

    88 没做出来

---
+++ Problem88/src/main/java/Solution.java
@@ -0,0 +1,8 @@
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int i = 0, j = 0;
+        for (i = 0, j = 0; i < m && j < n; ) {
+
+        }
+    }
+}
---
+++ Problem88/src/test/java/SolutionTest.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class SolutionTest {
+    @Test
+    void test1() {
+        Assertions.assertEquals(1, 1);
+    }
+}
